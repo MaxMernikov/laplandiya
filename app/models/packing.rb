16e1f50e3e1bcdd6f8d32af6da47b22e895b39c2
max mernikov <maxmernikov@gmail.com>
2015-10-30 10:51:45 +0300

    fx

--- app/models/packing.rb
+++ app/models/packing.rb
@@ -1,7 +1,9 @@
 class Packing < ActiveRecord::Base
   validates :image, attachment_presence: true
-  has_attached_file :image
+  has_attached_file :image, styles: { preview: '240x150>', big: '420x420>' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  
 
   has_many :packings_weights
   has_many :weights, through: :packings_weights
