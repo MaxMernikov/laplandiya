af6ef0aeff23182b6c3baeb5db15506883fd444a
max mernikov <maxmernikov@gmail.com>
2015-11-05 12:32:38 +0300

    d

--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -1,13 +1,6 @@
 class KitsController < ApplicationController
   def show
     set_meta('kit_page')
-    
-    if cookies[:recently_viewed].present?
-      recently_viewed = JSON.parse(cookies[:recently_viewed])
-      cookies[:recently_viewed] = JSON.generate( recently_viewed.push(params[:id]).uniq )
-    else
-      cookies[:recently_viewed] = JSON.generate([params[:id]])
-    end
 
     code = Kit.parse_code(params[:id])
     @compositions = Composition.ordered
@@ -15,5 +8,12 @@ class KitsController < ApplicationController
     @weight = code[:weight]
     @packing = code[:packing]
     @kit = Kit.find_by(composition_id: @composition.id, weight_id: @weight.id)
+
+    if cookies[:recently_viewed].present?
+      recently_viewed = JSON.parse(cookies[:recently_viewed])
+      cookies[:recently_viewed] = JSON.generate( recently_viewed.push(params[:id]).uniq )
+    else
+      cookies[:recently_viewed] = JSON.generate([params[:id]])
+    end
   end
 end
